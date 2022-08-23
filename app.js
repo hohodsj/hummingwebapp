ba98f70e8af23b63fe76be297fbb7785ffd0a118
hohodsj <hohodsj@gmail.com>
2022-08-22 22:15:34 -0400

    google image upload

--- app.js
+++ app.js
@@ -15,10 +15,15 @@ const {isLoggedIn} = require('./middleware');
 const methodOverride = require('method-override');
 var {google} = require('googleapis')
 const multer = require("multer");
-const {storage} = require('./cloudinary');
+// const {storage} = require('./cloudinary');
+const storage = multer.memoryStorage();
 const upload = multer({storage});
 // const upload = multer({dest: 'uploads/'});
 const {createCollection} = require("./controllers/collectionsController");
+const sharp = require('sharp');
+const fs = require('fs');
+const GoogleDriveStorage = require('multer-google-drive');
+
 
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/hummingsang-portfolio";
 mongoose.connect(dbUrl);
@@ -96,7 +101,7 @@ app.get('/admin/portfolio', isLoggedIn, async(req, res) => {
     res.render('admin/edit-portfolio', {collections});
 })
 
-app.get('/admin/create-collection', isLoggedIn, async(req, res) => {
+app.get('/admin/create-collection', async(req, res) => {
     res.render('admin/create-collection');
 })
 
@@ -105,7 +110,59 @@ app.get('/admin/create-collection', isLoggedIn, async(req, res) => {
 //     res.redirect('/admin/portfolio');
 // })
 
-app.post('/admin/create-collection', isLoggedIn, upload.array('image'), createCollection);
+// app.post('/admin/create-collection', isLoggedIn, upload.array('image'), createCollection);
+app.post('/admin/create-collection', upload.single('image'), async (req, res) => {
+    console.log('file', req.file);
+    console.log('body', req.body);
+    fs.access('./uploads', (err) => {
+        if(err) {
+            fs.mkdir('./uploads');
+        }
+    })
+    await sharp(req.file.buffer).resize({width: 100, height: 100}).toFile('./uploads/' + req.file.originalname);
+});
+
+app.get('/gd', async (req,res) => {
+    const clientId = process.env.GOOGLE_DRIVE_CLIENT_ID;
+    const clientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET;
+    const redirectUrl = process.env.GOOGLE_DRIVE_REDIRECT_URL;
+    const refreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN;
+    const client = new google.auth.OAuth2(clientId, clientSecret, redirectUrl);
+    client.setCredentials({ refresh_token: refreshToken });
+    const drive = google.drive({version: 'v3', auth:client});
+    try{
+        /*
+        const folderRsp = await drive.files.create({
+            resource: {
+                name: 'test',
+                mimeType: 'application/vnd.google-apps.folder',
+            },
+            fields: 'id,name',
+        });
+        console.log(`id: ${folderRsp.data.id}`);
+        */
+
+        // able to upload, need to put them in folder, and public accessiable
+        const response = await drive.files.create({
+            requestBody: {
+                name:"a.png",
+                mimeType: "image/png",
+                //parents: ['1GHTRmOK7cJcNjw_LqJU6Nve4s20Mn0RU']
+                parents: ['1cMJU3pMr110fmsIMRIgwCMozEF_uzeIY']
+            },
+            media: {
+                mimeType: "image/png",
+                body: fs.createReadStream("./uploads/a.png")
+            }
+        })
+        console.log(response.data);
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+    res.redirect('/');
+})
+
 
 app.delete('/:collection', isLoggedIn, async(req, res) => {
     const {collection} = req.params;
