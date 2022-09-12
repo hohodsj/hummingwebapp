b690963e5029270465722e557912466d1f681e63
hohodsj <hohodsj@gmail.com>
2022-09-11 22:10:02 -0400

    fix cover, image upload and text

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,8 @@ const upload = multer({storage});
 const googleDriveUtil = require('../utils/googleDriveUtil');
 const {removeCollection} = require('../utils/deleteUtils');
 const catchAsync = require('../utils/catchAsync');
-const { ids } = require('googleapis/build/src/apis/ids');
+const descriptionSchema = require('../models/descriptionSchema');
+const collectionSchema = require('../models/collectionSchema');
 
 router.get('/portfolio', async(req, res) => {
     const collections = await CollectionSchema.find({}).populate('cover').sort({order:1});
@@ -70,6 +71,7 @@ router.route('/create-collection')
             description: description,
             updateDate: new Date()
         });
+        artworks.push(cover);
         for (const artwork of artworks) {
             artwork.collectionSchema = collection;
             artwork.save();
@@ -99,22 +101,90 @@ router.post('/reorder/portfolio', async(req, res) => {
     res.redirect(`/admin/portfolio`);
 })
 
-router.post('/reorder/artwork/:collectionId/:collectionName', async(req, res) => {
-    const ordersArr = req.body.orders.split(',');
-    console.log(ordersArr);
-    for(let i = 0; i < ordersArr.length; i++) {
-        const oriOrder = ordersArr[i];
-        //if(i != oriOrder) {
-            console.log(`update origin order ${oriOrder} to ${i}`);
-            ArtWorkSchema.findOneAndUpdate(
-                {collectionSchema: req.params.collectionId, order: oriOrder}, 
-                {$set: {order: i}},
-                 function (err, docs) {
-                if(err) {
-                    req.flash('error', `Unable to update order ${oriOrder} to ${i} , Error: ${err}`);
-                } 
+router.post('/update/collection/:collectionId/:collectionName', upload.fields([{name:'cover'}, {name: 'image'}]), isCollectionExists, createUploadFolder, async(req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const collection = await CollectionSchema.findOne({_id:req.params.collectionId}).populate('description').populate('cover');
+    const descriptionId = collection.description._id;
+    const collectionId = req.params.collectionId;
+    const collectionName = req.params.collectionName;
+    descriptionSchema.findOneAndUpdate(
+        {_id: descriptionId},
+        {$set: {title: title, description:description}},
+        function (err, docs) {
+            if(err) {
+                console.log(err)
+            } else {
+                console.log(docs)
+            }
+        }
+    )
+    if(req.body.orders) {
+        const ordersArr = req.body.orders.split(',');
+        console.log(ordersArr);
+        for(let i = 0; i < ordersArr.length; i++) {
+            const oriOrder = ordersArr[i];
+            if(i != oriOrder) {
+                console.log(`update origin order ${oriOrder} to ${i}`);
+                ArtWorkSchema.findOneAndUpdate(
+                    {collectionSchema: collectionId, order: oriOrder}, 
+                    {$set: {order: i}},
+                    function (err, docs) {
+                    if(err) {
+                        req.flash('error', `Unable to update order ${oriOrder} to ${i} , Error: ${err}`);
+                    } 
+                })
+            }
+        }
+    }
+    if(req.files.cover) {
+        const file = req.files.cover[0];
+        const [thumbnailId, imageId, isHorizontal] = await googleDriveUtil.uploadImageToDrive(file.buffer, file.originalname.split('.').pop());
+        const cover = new ArtWorkSchema({
+            thumbnailId: thumbnailId,
+            imageId: imageId,
+            fileName: file.originalname,
+            isHorizontal: isHorizontal,
+            order: -1,
+            createDate: new Date(),
+            collectionSchema: collection
+        })
+        await cover.save();
+        const oldCover = collection.cover;
+        await googleDriveUtil.deleteImageWithIds([oldCover.thumbnailId, oldCover.imageId]);
+        ArtWorkSchema.remove({_id: oldCover._id});
+        collection.cover = cover;
+        await collection.save();
+    }
+    
+    if(req.files.image) {
+        for(let i = 0; i < req.files.image.length; i++) {
+            const file = req.files.image[i];
+            const [thumbnailId, imageId, isHorizontal] = await googleDriveUtil.uploadImageToDrive(file.buffer, file.originalname.split('.').pop());
+            // const collectionCount = await CollectionSchema.countDocuments({});
+            const artworksCount = await ArtWorkSchema.countDocuments({collectionSchema: collectionId});
+            const artwork = new ArtWorkSchema({
+                thumbnailId: thumbnailId,
+                imageId: imageId,
+                fileName: file.originalname,
+                order: artworksCount,
+                isHorizontal: isHorizontal,
+                createDate: new Date(),
+                collectionSchema: collection,
             })
-        //}
+            await artwork.save();
+            CollectionSchema.updateOne(
+                {_id: collectionId},
+                {$push: {artworks: artwork}},
+                function (err, docs) {
+                    if(err) {
+                        console.log(err)
+                    } else {
+                        console.log(docs)
+                    }
+                }
+            );
+        }
     }
     res.redirect(`/admin/${req.params.collectionName}`);
 })
